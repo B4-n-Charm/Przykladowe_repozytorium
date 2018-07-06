bac6551ed2bbc26b83a45ae0f219be99d835a938
Ciurski <s.bidowaniec@gmail.com>
2018-07-06 16:00:28 +0200

    modify function 1, add function 5

--- testowy_plik_python.py
+++ testowy_plik_python.py
@@ -1,8 +1,13 @@
 def funkcja_pierwsza():
     a = 2 * 2
-    return a
+    a2 = 3 * 3
+    return a * a2
 
 
 def funkcja_druga():
     b = 4 * 4
     return b
+
+
+def funkcja_piata():
+    return "spam"
